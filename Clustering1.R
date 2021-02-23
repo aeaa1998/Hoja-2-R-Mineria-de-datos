44254995b035bc58ec4a8c8e83a5e5a42c2da37a
Joonho D. Kim <37599608+jkmolina@users.noreply.github.com>
2021-02-22 23:04:49 -0600

    Fuzzy C-Means Update

---
+++ Clustering1.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(cluster) # Para calcular la silueta
+library(e1071)# Para cmeans
+library(mclust) # Mixtures of gaussians
+library(fpc) # Para hacer el plotcluster
+library(NbClust) # Para determinar el n?mero de clusters ?ptimo
+library(factoextra) # Para hacer gr?ficos de clustering
+library(tidyr)
+
+movies <- read.csv("/tmdb-movies.csv", stringsAsFactors = F)
+movies <- na.omit(movies)
+
+#Inciso 1
+
+#Se obtienen las variables seleccionadas para trabajar
+clusteringVar <- movies[c("popularity","budget","revenue","runtime","vote_average")]
+
+#Inciso 2
+
+#Nos ayuda a conocer cual es el numero de clusters adecuados
+wss <- (nrow(clusteringVar)-1)*sum(apply(clusteringVar,2,var))
+for (i in 2:10) 
+  wss[i] <- sum(kmeans(clusteringVar, centers=i)$withinss)
+
+# Se plotea la grafica de codo
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
+
+#Fuzzy C-Means
+fcm<-cmeans(clusteringVar,4)
+datos$FCGrupos<-fcm$cluster
+peliculasCompleto$CM<-fcm$cluster
+datos<-cbind(datos,fcm$membership)
+
+# Silueta para Fuzzy C-Means
+silfcm<-silhouette(fcm$cluster,dist(clusteringVar))
+mean(silfcm[,3])
+plotcluster(clusteringVar,fcm$cluster) 
+
