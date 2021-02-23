e6750cce050c25d83800ff5f9eea9792d46ff575
Mario9810 <32363631+Mario9810@users.noreply.github.com>
2021-02-22 22:40:50 -0600

    Update clustering.R

--- clustering.R
+++ clustering.R
@@ -5,7 +5,8 @@ library(fpc) #para hacer el plotcluster
 library(NbClust) #Para determinar el n?mero de clusters ?ptimo
 library(factoextra) #Para hacer gr?ficos bonitos de clustering
 library(VIM)
-
+library("ggpubr")
+library("ggplot2")
 
 #clustering
 #k-meanss
@@ -33,3 +34,21 @@ plotcluster(datos[,1:5],km$cluster)
 
 silkm<-silhouette(km$cluster,dist(datos[,1:5]))
 mean(silkm[,3])
+
+#Visualizacion de las k-medias
+fviz_cluster(km, data = datos[,1:5],geom = "point", ellipse.type = "norm")
+
+#tendencia central 
+mean(g1$popularit)
+median(g1$popularity)
+str(datos)
+
+ggdensity(datos$runtime, 
+          main = "runtime",
+          xlab = "time")
+#correlaciones 
+cor(datos$budget,datos$revenue, method = c("pearson", "kendall", "spearman"))
+
+cor(datos$revenue,datos$runtime, method = c("pearson", "kendall", "spearman"))
+
+cor(datos$budget,datos$runtime, method = c("pearson", "kendall", "spearman"))
